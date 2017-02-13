ef7dd200a9695d9a8300580c69b8e6402406a895
Kevin <km0585@yp.com>
2017-02-13 15:46:17 -0800

    initial load

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+var smtpTransport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+      user: "kmenezes@gmail.com",
+      pass: "Jianada1"
+  }
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/send', function(req, res){
+  var mailOptions = {
+    from: req.query.from,
+    to: req.query.to,
+    subject: req.query.subject,
+    text: req.query.text
+  }
+  console.log(mailOptions);
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if (error){
+      res.send("error");
+    }
+    else{
+      console.log("Message sent: " + response.message);
+      res.send("sent");
+    }
+  })
+})
+
+module.exports = router;
