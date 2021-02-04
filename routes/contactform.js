fb82c0062a748d27fd58025c379dff3c241f9dab
Kevin Menezes <kevinmenezes@yahoo.com>
2021-02-03 17:17:41 -0800

    updated to use credentials

--- routes/contactform.js
+++ routes/contactform.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix'
-    //path: '/usr/sbin/sendmail'
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'cardelectricemail@gmail.com', // Your email id
+        pass: 'cardelectric1' // Your password
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    }
 });
 
 /* GET home page. */
@@ -14,29 +22,32 @@ router.get('/', function(req, res, next) {
     res.send('Get request received');
   });
   
-  router.post('/send', function(req, res){
-    var name = req.body.name;
-        
-    var mailOptions = {
-      from: 'admin@cardelectric.com',
-      to: 'cardelectric@yahoo.com',
-      subject: "Contact form submitted",
-      text: 'Name: ' + name + String.fromCharCode(13) + String.fromCharCode(13) + req.body.text
+router.post('/send', function(req, res){
+var name = req.body.name;
+    
+var mailOptions = {
+    from: 'admin@cardelectric.com',
+    to: 'cardelectric@yahoo.com, kevinmenezes@yahoo.com',
+    subject: "Contact form submitted",
+    text: 'Name: ' + name + String.fromCharCode(13) + String.fromCharCode(13) + req.body.text,
+    auth: {
+
     }
-    console.log(mailOptions);
-  
-    res.setHeader('Access-Control-Allow-Origin','*');
-    transporter.sendMail(mailOptions, function(error, response){
-      if (error){
-        console.log('Error: ' + error);
-        res.end("error: " + error);
-      }
-      else{
-        console.log("Message sent");
-        res.end("sent");
-      }
-    })
-  })
+}
+console.log(mailOptions);
+
+res.setHeader('Access-Control-Allow-Origin','*');
+transporter.sendMail(mailOptions, function(error, response){
+    if (error){
+    console.log('Error: ' + error);
+    res.end("error: " + error);
+    }
+    else{
+    console.log("Message sent");
+    res.end("sent");
+    }
+})
+})
   
   module.exports = router;
   
