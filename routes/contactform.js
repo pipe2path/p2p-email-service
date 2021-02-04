032c379807cb7e9e42e8a11229324b308426fa63
Kevin Menezes <kevinmenezes@yahoo.com>
2021-02-03 22:56:58 -0800

    updated redirect site

--- routes/contactform.js
+++ routes/contactform.js
@@ -48,7 +48,7 @@ transporter.sendMail(mailOptions, function(error, response){
     res.end("error: " + error);
     }
     else{
-        res.redirect("/");
+        res.redirect("http://cardenaselectric.netlify.com");
     }
 })
 })
