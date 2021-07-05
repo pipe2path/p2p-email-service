1a2522517eb3685c711f29a4f6ee3c08adb37e2b
Kevin Menezes <kevinmenezes@yahoo.com>
2021-07-05 08:31:55 -0700

    not allowing 'undefined' in comments

--- routes/contactform.js
+++ routes/contactform.js
@@ -2,16 +2,13 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
-console.log('user: ' + process.env.USERNAME);
-console.log('pass: ' + process.env.PASSWORD);
-
 var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-        user: process.env.USERNAME, // Your email id
-        pass: process.env.PASSWORD // Your password
+        user: process.env.USERNAME, 
+        pass: process.env.PASSWORD 
     },
     tls: {
         // do not fail on invalid certs
@@ -19,39 +16,44 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-/* GET home page. */
+// GET
 router.get('/', function(req, res, next) {
-    //res.render('index', { title: 'Express' });
     res.send('Get request received');
   });
   
+// POST  
 router.post('/send', function(req, res){
-var name = req.body.name;
-    
-var mailOptions = {
-    from: 'admin@cardelectric.com',
-    to: 'kevinmenezes@yahoo.com, cardelectric@yahoo.com',
-    subject: "Contact form submitted",
-    text: 'Name: ' + name + String.fromCharCode(13) + String.fromCharCode(13) + req.body.phone + 
-                String.fromCharCode(13) + String.fromCharCode(13) + req.body.email + 
-            String.fromCharCode(13) + String.fromCharCode(13) + req.body.comments ,
-    auth: {
-
+    var name = req.body.name;
+    var comments = req.body.comments;
+    if (comments.includes('undefined')){
+        res.redirect("http://cardenaselectric.com/#contact_form");    
     }
+
+    var mailOptions = {
+        from: 'admin@cardelectric.com',
+        //to: 'kevinmenezes@yahoo.com, cardelectric@yahoo.com',
+        to: 'kevinmenezes@yahoo.com',
+        subject: "Contact form submitted",
+        text: 'Name: ' + name + String.fromCharCode(13) + String.fromCharCode(13) + req.body.phone + 
+                    String.fromCharCode(13) + String.fromCharCode(13) + req.body.email + 
+                String.fromCharCode(13) + String.fromCharCode(13) + req.body.comments ,
+        auth: {
+
+        }
 }
 console.log(mailOptions);
 
 res.setHeader('Access-Control-Allow-Origin','*');
-transporter.sendMail(mailOptions, function(error, response){
-    if (error){
-    console.log('Error: ' + error);
-    res.end("error: " + error);
-    }
-    else{
-        res.redirect("http://cardenaselectric.com/#contact_form");
-    }
-})
+    transporter.sendMail(mailOptions, function(error, response){
+        if (error){
+            console.log('Error: ' + error);
+            res.end("error: " + error);
+        }
+        else{
+            res.redirect("http://cardenaselectric.com/#contact_form");
+        }
+    })
 })
   
-  module.exports = router;
+module.exports = router;
   
